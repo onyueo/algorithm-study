83f01f0988dcab9c04afa553464b24e4ae36d739
On Yu <84468769+onyueo@users.noreply.github.com>
2024-09-07 00:29:03 +0900

    [level 0] Title: 다항식 더하기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,27 +1,31 @@
 function solution(polynomial) {
-    const terms = polynomial.split(' + '); // 다항식의 항을 분리
-    const coefficients = {}; // 동류항의 계수를 저장할 객체
-
-    for (const term of terms) {
-        if (term.includes('x')) {
-            const [coefficient] = term.split('x'); // 'x' 앞의 계수 추출
-            const num = coefficient === '' ? 1 : coefficient === '-' ? -1 : parseInt(coefficient); // 계수 파싱
-            coefficients['x'] = (coefficients['x'] || 0) + num; // 'x' 항의 계수 합산
+    var ans = 0;
+    let x = polynomial.split(' ').filter((el) => el.includes('x'))
+    let c = polynomial.split(' ').filter((el) => !el.includes('x') && !el.includes('+'))
+    for (num of x) {
+        if (num === 'x') {
+            ans += 1
         } else {
-            const constant = parseInt(term); // 상수항 파싱
-            coefficients['const'] = (coefficients['const'] || 0) + constant; // 상수항 합산
+            ans += Number(num.replace('x', ''))
         }
     }
 
-    // 결과 문자열 생성
-    let result = '';
-    if (coefficients['x']) {
-        result += coefficients['x'] === 1 ? 'x' : coefficients['x'] === -1 ? '-x' : coefficients['x'] + 'x';
+    c = c.reduce((arr, cur) => arr + +cur, 0)
+    if (c === 0) {
+        if (ans === 1) {
+            return 'x'
+        } else {
+            return ans+'x'
+        }
+    } else if (ans === 0) {
+        return String(c)
+    } else if (c === 0 && ans === 0){
+        return '0'
+    } else {
+        if (ans === 1) {
+            return 'x + '+ c
+        } else {
+            return ans +'x'+' + '+ c
+        }
     }
-    if (coefficients['const']) {
-        if (result) result += ' + '; // 이전에 계수가 있는 경우 + 추가
-        result += coefficients['const'];
-    }
-    
-    return result || '0'; // 모든 계수가 0일 경우 '0' 반환
 }
