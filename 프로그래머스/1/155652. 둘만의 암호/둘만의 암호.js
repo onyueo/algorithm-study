b81c170a8ccf027a3a74cf4f275d5f6ac55899c5
On Yu <84468769+onyueo@users.noreply.github.com>
2025-01-03 01:24:00 +0900

    [level 1] Title: 둘만의 암호, Time: 0.73 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,24 +1,19 @@
 function solution(s, skip, index) {
-  let ans = "";
-  let arr = s.split("");
-  // a: 97 z: 122
-  arr.map((item) => {
-    let a = item.charCodeAt(); // 아스키코드로 나타냄
-    for (let i = 0; i < index; i++) {
-      a++; // 숫자 1더한 값으로 아래 사항들 수행
-      // z 이상이면 a로 바꿈
-      if (a > 122) {
-        a = 97;
-      }
-      // 다시 문자로 변환해서 skip에 들어있는지 확인
-      while (skip.includes(String.fromCodePoint(a))) {
-        a++; // 있다면 한번 더 +1
-        if (a > 122) {
-          a = 97;
+    var answer = '';
+    
+    for (ltt of [...s]) {
+        var alph = ltt.charCodeAt()
+        var num = 0
+        while(num < index) {
+            alph++
+            if (alph > 122) {
+                alph = 97
+            }
+            if (![...skip].includes(String.fromCharCode(alph))) {
+                num++
+            }
         }
-      }
+        answer += String.fromCharCode(alph)
     }
-    ans = ans + String.fromCodePoint(a);
-  });
-  return ans;
+    return answer;
 }
